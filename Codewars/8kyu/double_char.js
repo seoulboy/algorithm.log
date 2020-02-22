5c3609d922906c0e59ccd2d47af32db2d8e68602
oyvvl <940709@gmail.com>
2020-02-22 16:04:34 +0900

    added link and explanation

--- Codewars/8kyu/double_char.js
+++ Codewars/8kyu/double_char.js
@@ -1,17 +1,30 @@
 /*
+https://www.codewars.com/kata/56b1f01c247c01db92000076
 
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
+Good Luck!
 */
 
-// First Attempt - Using Array.split, Array.map, Array.join
+// First Attempt - 
 
-const doubleChar = (str) => str.split("").map(c => c + c).join("");
+const doubleChar = str =>
+	str
+		.split('')
+		.map(c => c + c)
+		.join('');
 
-// Second Attempt 
+// Second Attempt - Using String.split(), Array.map(), Array.join()
 
 function doubleChar(str) {
-  let newStr = ''
-  for (let i = 0; i < str.length; i++) {
-    newStr = newStr + str[i] + str[i]
-  }
-  return newStr
+	let newStr = '';
+	for (let i = 0; i < str.length; i++) {
+		newStr = newStr + str[i] + str[i];
+	}
+	return newStr;
 }
