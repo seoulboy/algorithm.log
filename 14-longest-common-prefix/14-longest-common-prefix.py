92d6f776421fffa8d8dd9a7bf7aff14371971212
Eric Imho Jang <940709@gmail.com>
2022-05-29 20:06:44 +0900

    Time: 59 ms (26.39%), Space: 13.9 MB (88.40%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -10,12 +10,10 @@ class Solution:
             commonPrefix = strs[0][:commonPrefixCount + 1]
             
             for string in strs:
-                print(string, commonPrefix, commonPrefixCount, len(string))
                 if commonPrefixCount >= len(string):
                     hasCommonPrefix = False
                 hasCommonPrefix = commonPrefix == string[:commonPrefixCount + 1] and hasCommonPrefix
                 
-            print(hasCommonPrefix)
             if hasCommonPrefix:
                 commonPrefixCount += 1
         
