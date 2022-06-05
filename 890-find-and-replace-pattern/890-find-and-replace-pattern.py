ca3aa810834ca87b63eafefebc85b907cd6a1319
Eric Imho Jang <940709@gmail.com>
2022-06-05 19:52:30 +0900

    Time: 71 ms (7.97%), Space: 13.9 MB (76.99%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -4,22 +4,18 @@ class Solution:
         
         for i in range(len(words)):
             dic = {}
-            word = words[i]
-            
-            # if len(word) != len(pattern):
-            #     continue
             
             for j in range(len(pattern)):
-                char = word[j]
+                char = words[i][j]
                 patternChar = pattern[j]
                 
                 if patternChar in dic.keys():
-                    if dic[patternChar] != char:
+                    if dic[patternChar] is not char:
                         break
                 else:
                     dic[patternChar] = char
                     
-                    if len(dic.values()) != len(set(dic.values())):
+                    if len(dic.values()) is not len(set(dic.values())):
                         break
                     
                 if j == len(pattern) - 1:
