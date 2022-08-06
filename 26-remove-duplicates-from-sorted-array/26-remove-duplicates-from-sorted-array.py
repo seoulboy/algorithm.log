46c113643093492eea1b2cac064ce1748b10e6d3
Eric Imho Jang <940709@gmail.com>
2022-08-06 21:23:06 +0900

    Time: 135 ms (62.09%), Space: 15.6 MB (61.41%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -4,13 +4,10 @@ class Solution:
             return 0
         
         previous = nums[0]
-        
         index = 1
-        length = 1
         for i in range(1, len(nums)):
             if previous != nums[i]:
                 nums[index] = nums[i]
                 index += 1
-                length += 1
             previous = nums[i]
-        return length
+        return index
