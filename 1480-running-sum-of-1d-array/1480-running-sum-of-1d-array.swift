035b99943b79f130fa82eea6f8117bd9b0762d89
Eric Imho Jang <940709@gmail.com>
2022-08-17 16:53:37 +0900

    Time: 11 ms (96.51%), Space: 14.2 MB (47.55%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var list: [Int] = []
+        var sum = 0
+        nums.forEach { 
+            sum += $0
+            list.append(sum)
+        }
+        return list
+    }
+}
