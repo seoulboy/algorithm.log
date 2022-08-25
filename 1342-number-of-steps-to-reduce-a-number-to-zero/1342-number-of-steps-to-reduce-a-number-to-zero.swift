73258bd3a6ba57f7050ed81bd79965ea1f535aad
Eric Imho Jang <940709@gmail.com>
2022-08-25 18:30:48 +0900

    Time: 4 ms (48.13%), Space: 13.6 MB (68.75%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func numberOfSteps(_ num: Int) -> Int {
+        var currentNum = num
+        var count = 0
+        while currentNum > 0 {
+            if currentNum % 2 == 0 {
+                currentNum /= 2
+            } else {
+                currentNum -= 1
+            }
+            count += 1
+        }
+        return count
+    }
+}
