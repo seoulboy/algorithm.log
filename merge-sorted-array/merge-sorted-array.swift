ebd5027226a32e00b4a6119a1087d7ed1c93e900
Eric Imho Jang <940709@gmail.com>
2022-09-12 14:05:50 +0900

    Time: 162 ms, Memory: 14.4 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.swift
+++ merge-sorted-array/merge-sorted-array.swift
@@ -1,9 +1,40 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        for i in (m..<(m+n)) {
-            nums1[i] = nums2[i - m]   
+        if n == 0 {
+            return
         }
         
-        nums1 = nums1.sorted()
+        var idx1 = 0
+        var idx2 = 0
+        
+        var newArr = [Int]()
+        
+        for i in (0..<(m+n)) {
+            if idx1 == m {
+                newArr.append(nums2[idx2])
+                idx2 += 1
+                continue
+            } else if idx2 == n {
+                newArr.append(nums1[idx1])
+                idx1 += 1
+                continue
+            }
+            
+            var elem1 = nums1[idx1]
+            var elem2 = nums2[idx2]
+            
+            if elem1 > elem2 {
+                newArr.append(elem2)
+                print("append elem2: \(elem2), newArr: \(newArr)")
+                idx2 += 1
+            } else {
+                newArr.append(elem1)
+                print("append elem1: \(elem1), newArr: \(newArr)")
+                idx1 += 1
+            }
+            
+        }
+        
+        nums1 = newArr
     }
 }
