7337f21e80b730338cce35e4df91e723c9349450
hoftware <940709@gmail.com>
2025-03-17 16:32:27 +0900

    Time: 13 ms (1.06%), Space: 19.9 MB (34.56%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        let cols = matrix[0].count, rows = matrix.count
+
+        var targetRow = [Int]()
+
+        for row in matrix where target <= row[cols-1] {
+                targetRow = row
+                break
+        }
+
+        var left = 0, right = targetRow.count
+        
+        while left < right {
+            var mid = left + (right-left)/2
+
+            if targetRow[mid] == target {
+                return true
+            } else if targetRow[mid] < target {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+
+        return false
+    }
+}
