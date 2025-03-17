2b9e41e0682c7e2c9e5fdee5a55e14e400a94953
hoftware <940709@gmail.com>
2025-03-17 15:24:55 +0900

    Time: 0 ms (100%), Space: 19.5 MB (80.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.swift
+++ 0035-search-insert-position/0035-search-insert-position.swift
@@ -1,26 +1,19 @@
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        var left = 0, right = nums.count-1
-        var result = -1
+        var left = 0, right = nums.count
 
-        while true {
-            guard left <= right else {
-                result = left
-                break
-            }
+        while left < right {
+            var mid = left + (right-left)/2
 
-            let mid = left + (right-left)/2
-            
             if nums[mid] == target {
-                result = mid
-                break
+                return mid
             } else if nums[mid] < target {
                 left = mid + 1
-            } else if nums[mid] > target {
-                right = mid - 1
+            } else {
+                right = mid
             }
         }
 
-        return result
+        return left
     }
 }
