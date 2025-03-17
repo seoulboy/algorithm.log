7fa08827296fc6ceaccffe79205c2f0abd5573e0
hoftware <940709@gmail.com>
2025-03-17 16:39:46 +0900

    Time: 0 ms (100%), Space: 19.8 MB (43.8%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
@@ -1,30 +1,49 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        let cols = matrix[0].count, rows = matrix.count
+        let colCount = matrix[0].count
+        let rowCount = matrix.count
 
-        guard matrix[rows-1][cols-1] >= target else { return false }
+        guard matrix[rowCount-1][colCount-1] >= target else { return false }
 
-        var targetRow = [Int]()
-
-        for row in matrix where target <= row[cols-1] {
-                targetRow = row
-                break
-        }
-
-        var left = 0, right = targetRow.count
+        var left = 0, right = colCount * rowCount
         
         while left < right {
-            var mid = left + (right-left)/2
+            let mid = left + (right-left)/2
+            let row = mid / colCount 
+            let col = mid % colCount
 
-            if targetRow[mid] == target {
+            if matrix[row][col] == target {
                 return true
-            } else if targetRow[mid] < target {
-                left = mid + 1
+            } else if matrix[row][col] < target {
+                left = mid+1
             } else {
                 right = mid
             }
         }
 
         return false
+
+        // var targetRow = [Int]()
+
+        // for row in matrix where target <= row[cols-1] {
+        //         targetRow = row
+        //         break
+        // }
+
+        // var left = 0, right = targetRow.count
+        
+        // while left < right {
+        //     var mid = left + (right-left)/2
+
+        //     if targetRow[mid] == target {
+        //         return true
+        //     } else if targetRow[mid] < target {
+        //         left = mid + 1
+        //     } else {
+        //         right = mid
+        //     }
+        // }
+
+        // return false
     }
 }
