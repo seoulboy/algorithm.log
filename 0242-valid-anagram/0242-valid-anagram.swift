957c96925bbccbe0429f07021c033da2ef7f017a
hoftware <940709@gmail.com>
2025-09-18 13:27:23 +0900

    Time: 1 ms (95.93%), Space: 19.7 MB (91.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.swift
+++ 0242-valid-anagram/0242-valid-anagram.swift
@@ -1,13 +1,16 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
+        var s = Array(s.unicodeScalars)
+        var t = Array(t.unicodeScalars)
+        
         var a = Array(repeating: 0, count: 26)
         
-        for char in Array(s.unicodeScalars) {
+        for char in s {
             let index = Int(char.value) - 97
             a[index] += 1 
         }
         
-        for char in Array(t.unicodeScalars) {
+        for char in t {
             let index = Int(char.value) - 97
             a[index] -= 1 
         }
