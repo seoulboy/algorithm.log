967c76a008dc13be0d8d25d2e17cd171354b0ca7
hoftware <940709@gmail.com>
2025-09-19 15:02:00 +0900

    Time: 0 ms (100%), Space: 20 MB (24.07%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.swift
+++ 0118-pascals-triangle/0118-pascals-triangle.swift
@@ -1,23 +1,18 @@
 class Solution {
     func generate(_ numRows: Int) -> [[Int]] {
-        
         var a: [[Int]] = []
         
-        var i = 0
-        while i < numRows {
+        for i in 0..<numRows {
             var c = Array(repeating: 0, count: i+1)
             
             for j in c.indices {
                 if j == 0 || j == c.count-1 {
                     c[j] = 1
-                    continue
+                } else {
+                    c[j] = a[i-1][j-1] + a[i-1][j]
                 }
-                
-                c[j] = a[i-1][j-1] + a[i-1][j]
             }
             a.append(c)
-            
-            i += 1
         }
         return a
     }
