2fbdacafc655e9c9c57e8c7c1fa06a7a2565c692
hoftware <940709@gmail.com>
2025-09-19 14:56:13 +0900

    Time: 0 ms (100%), Space: 20.1 MB (12.85%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func generate(_ numRows: Int) -> [[Int]] {
+        
+        var a: [[Int]] = []
+        
+        var i = 0
+        while i < numRows {
+            var c = Array(repeating: 0, count: i+1)
+            
+            for j in c.indices {
+                if j == 0 || j == c.count-1 {
+                    c[j] = 1
+                    continue
+                }
+                
+                c[j] = a[i-1][j-1] + a[i-1][j]
+            }
+            a.append(c)
+            
+            i += 1
+        }
+        return a
+    }
+}
